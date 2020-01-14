5a74dd9a23fe3b2e86385f4ad6689d6aba04b0cd
Julian <julianofhernandez@gmail.com>
2020-01-14 14:11:16 -0800

    Created stock ticker for top of webpage

---
+++ stocks.js
@@ -0,0 +1,26 @@
+var apikey = "BYMNX5T5A8BD5U1T";
+var stockList = ["AMZN","AAPL","MSFT","VTI","VOO"]
+
+function createStockDiv(ticker, price, change) {
+    //Create new ticker div
+    var stockDiv = document.createElement("div");
+    stockDiv.id = ticker;
+    stockDiv.innerText = ticker+" "+price+" "+change;
+
+    //Append ticker to list
+    var currentDiv = document.getElementById("ticker-text");
+    currentDiv.appendChild(stockDiv);
+}
+
+function getStockData(ticker) {
+    var url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="+ticker+"&apikey="+apikey
+    $.getJSON(url, "", function(data) {
+        console.log(data)
+        price = data["Global Quote"]["05. price"]
+        change = data["Global Quote"]["09. change"]
+        createStockDiv(ticker, price, change);
+    })
+}
+
+stockList.forEach(element => {getStockData(element)})
+
